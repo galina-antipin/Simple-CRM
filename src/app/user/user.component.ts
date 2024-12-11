d6f069ed927604c502292643672a5b206d77cdac
Galina Antipin <galina.antipin@gmail.com>
2024-12-11 10:50:49 +0100

    changed addUserBtn position and added matToolTip message

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterLink, CommonModule, MatButtonModule, MatIconModule],
+  imports: [RouterLink, CommonModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
