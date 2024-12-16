d81042b9fc724760b3d6dcb5d9116d04fc4978ba
Galina Antipin <galina.antipin@gmail.com>
2024-12-16 13:14:44 +0100

    added new components user and dashboard and try to impliments an dialog field

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,14 +4,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterLink, CommonModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [RouterLink, CommonModule, MatButtonModule, MatIconModule, MatTooltipModule, MatDialog, MatDialogModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
 
+  constructor(public dialog: MatDialog){}
+
+  openDialog(){
+    this.dialog.open(DialogAddUserComponent)
+  }
 }
