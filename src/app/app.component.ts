a9938c2db5f0e37ae5f715cdc3c342a8df07f73b
Galina Antipin <galina.antipin@gmail.com>
2025-01-07 10:07:52 +0100

    added a daypicker to enter your birth date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet, 
-    MatToolbarModule, 
-    MatInputModule, 
-    MatSidenavModule, 
-    MatDialogModule, 
-    DashboardComponent, 
-    UserComponent, 
-    RouterLink, 
-    MatIconModule, 
+    RouterOutlet,
+    MatToolbarModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatDialogModule,
+    DashboardComponent,
+    UserComponent,
+    RouterLink,
+    MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
-  DialogAddUserComponent],
-  
+    DialogAddUserComponent,
+    MatNativeDateModule],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
