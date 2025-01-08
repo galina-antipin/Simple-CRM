ed991b32d8132389481db3804800c3fd4f2e07f6
Galina Antipin <galina.antipin@gmail.com>
2025-01-08 08:18:26 +0100

    added ngModel on input fields

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { User } from '../../models/user.class';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -14,12 +16,13 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-     MatDatepickerModule,
-     MatNativeDateModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+  user = new User();
 
 }
